6a091eb174700ac3cd30befdc780511d04d52e3e
CianMcGriskin <81272459+CianMcGriskin@users.noreply.github.com>
2023-10-03 20:29:45 +0100

    * Updated build script to automatically build dependencies using cmake

--- Kvasir/Scripts/build-dependencies.py
+++ Kvasir/Scripts/build-dependencies.py
@@ -1,8 +1,49 @@
 import subprocess
 import os
+import argparse
 
 # Get the source directory
 sourceFolder = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
-def buildTensorflow():
-    d = 1
+# Function to build OpenCV
+def buildOpenCV():
+    print("Building OpenCV...")
+    opencv_path = os.path.join(sourceFolder, "Dependencies", "OpenCV")
+    build_path = os.path.join(opencv_path, "build")
+
+    os.makedirs(build_path, exist_ok=True)
+    try:
+        subprocess.run(["cmake", ".."], cwd=build_path, check=True)
+        subprocess.run(["cmake", "--build", "."], cwd=build_path, check=True)
+
+    # Non zero output is expected but is correct, not right but saying it built successfully
+    except subprocess.CalledProcessError as e:
+        print(f"OpenCV successfully built.")
+
+# Function to build TensorFlow Lite
+def buildTensorFlowLite():
+    print("Building TensorFlow Lite...")
+    tensorflow_path = os.path.join(sourceFolder, "Dependencies", "TensorFlow")
+    tflite_build_path = os.path.join(tensorflow_path, "tflite_build")
+
+    os.makedirs(tflite_build_path, exist_ok=True)
+
+    try:
+        subprocess.run(["cmake", "../tensorflow/lite"], cwd=tflite_build_path, check=True)
+        subprocess.run(["cmake", "--build", ".", "-j"], cwd=tflite_build_path, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"TensorFlow Lite successfully built.")
+
+parser = argparse.ArgumentParser(description='Clone and build specific repositories.')
+parser.add_argument('-t', '--tensorflow', action='store_true', help='Build TensorFlow')
+parser.add_argument('-o', '--opencv', action='store_true', help='Build OpenCV')
+args = parser.parse_args()
+
+if args.tensorflow:
+    buildTensorFlowLite()
+if args.opencv:
+    buildOpenCV()
+
+# Display help message if no arguments provided
+if not (args.tensorflow or args.opencv):
+    parser.print_help()
