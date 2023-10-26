6810d592f46e2ca7719f80f582bf317f86700821
Adrian-Holk-Lebinski <G00395620@gmit.ie>
2023-10-26 22:49:12 +0100

    Completed Live Stream page that shows live stream footage from a local device

--- Kvasir/src/app/livestream/livestream.page.ts
+++ Kvasir/src/app/livestream/livestream.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit} from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { HttpClient } from '@angular/common/http';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-livestream',
@@ -8,38 +9,41 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./livestream.page.scss'],
 })
 export class LivestreamPage implements OnInit{
-  mjpegUrl: SafeUrl;
+
+  streamUrl: string = 'http://192.168.1.8:8080';
   isCameraOffline: boolean = false;
+  streamImage: any;
+
+  private subscription: Subscription | undefined;
  
   constructor(private sanitizer: DomSanitizer, private http: HttpClient) {
-    this.mjpegUrl = this.sanitizer.bypassSecurityTrustUrl('http://192.168.1.8:8080/');
-  }
-  handleImageError() {
-    // This function will be called when the image fails to load
-    this.isCameraOffline = true;
+    this.streamImage = new Image();
   }
 
   ngOnInit() {
     // Check the stream status initially
     this.checkStreamStatus();
 
-    // Poll the server every 5 seconds (adjust the interval as needed)
-    setInterval(() => {
+    this.subscription = interval(500).subscribe(() => {
       this.checkStreamStatus();
-    }, 5000);
+    });
   }
 
   checkStreamStatus() {
-    const img = new Image();
-    img.onload = () => {
-      // Image loaded successfully, stream is online
-      this.isCameraOffline = false;
+    this.streamImage.src = this.streamUrl;
+
+    this.streamImage.onload = () => {
+      console.log(this.streamImage.width + " " + this.streamImage.height)
+      if (this.streamImage.width === 0) {
+        this.isCameraOffline = true;
+      } else {
+        this.isCameraOffline = false;
+      }
     };
-    img.onerror = () => {
-      // Error occurred, camera is offline
+
+    this.streamImage.onerror = () => {
       this.isCameraOffline = true;
     };
-    img.src = this.mjpegUrl.toString() + '?' + new Date().getTime(); // Append a timestamp to prevent caching
   }
 }
  
